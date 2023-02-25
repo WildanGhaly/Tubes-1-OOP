ab7131bb6a5e587bb057db3be33d9bfd944095f0
Hidayatullah Wildan Ghaly Buchary <16521502@std.stei.itb.ac.id>
2023-02-25 22:27:53 +0700

    feat: runable main, you can run main.exe if u want

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,18 @@
 // Halo ges pakabar?
+// Ini adalah main.cpp
+
+#include "Class/Game/Game.cpp"
+
+int main(){
+    std::cout << "Deck before start" << std::endl;
+    
+    Game game(7); // 7 player
+    game.print(); // print sebelum
+
+    game.start(2); // 2 kartu
+    std::cout << "Deck after start: " << game.getTotalCard() << std::endl;
+    
+    game.print(); // print setelah
+
+    return 0;
+}
