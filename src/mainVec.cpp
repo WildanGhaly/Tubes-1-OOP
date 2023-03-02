3c8322658686bbfd538764a3179810f5ed6e136f
AlphaThrone <haikalardzi@gmail.com>
2023-03-02 18:04:13 +0700

    main unfinished

--- src/mainVec.cpp
+++ src/mainVec.cpp
@@ -4,17 +4,33 @@
 #include "Class/Game/GameVec.cpp"
 
 int main(){
-    std::cout << "Deck before start" << std::endl;
-    
-    GameVec game(7); // 7 player
-    game.print(); // print sebelum
+    int chooseGame=0;
+    int round=0;
+    std::string next;
+    do{
+        cout << "Pilih Game: " << endl;
+        cout << "1. Poker" << endl;
+        cout << "2. camgkul" << endl;
+        cout << ">> ";
+        cin >> chooseGame;
+        if(chooseGame == 1){
+        std::cout << "Deck before start" << std::endl;
+        GameVec game(7); // 7 player
+        // game.print(); // print sebelum
+        game.start(2); // 2 kartu
+        while(round<5){ 
+            game.nextRound();
+            std::cout << "Next Round: (Y/n)" << std::endl;
+            std::cin >> next;
+            game.getTable().print();
+            if(next=="Y" || next=="y"){
+                round++;
+            }
+        }
 
-    game.start(6); // 2 kartu
-    game.nextRound();
-    game.nextRound();
-    std::cout << "Deck after start: " << game.getTotalCard() << std::endl;
-    
-    game.print(); // print setelah
-
+        std::cout << "Deck after start: " << game.getTotalCard() << std::endl;
+        game.getTable().print();
+        }
+    }while(true);
     return 0;
 }
