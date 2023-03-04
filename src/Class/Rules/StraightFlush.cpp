91e871dd65d65815dcbec5624ec2932088cfd568
archmans <13521010@std.stei.itb.ac.id>
2023-03-04 16:47:05 +0700

    Feat : StraightFlush

--- src/Class/Rules/StraightFlush.cpp
+++ src/Class/Rules/StraightFlush.cpp
@@ -40,27 +40,62 @@ void StraightFlush::setScore(float scoreNew) {
 }
 
 /* Method */
-/*! @attention Sorted by number needed 
-*/
-bool StraightFlush::isStraightFlush(std::vector<Card> cardComb, int& total) {
-    total = 0;
-    for (int i = 0; i < 3; i++) {
-        if (cardComb[i].getNumber() == cardComb[i+1].getNumber() - 1 && cardComb[i].getColor() == cardComb[i+1].getColor() &&
-            cardComb[i].getNumber() == cardComb[i+2].getNumber() - 2 && cardComb[i].getColor() == cardComb[i+2].getColor() &&
-            cardComb[i].getNumber() == cardComb[i+3].getNumber() - 3 && cardComb[i].getColor() == cardComb[i+3].getColor() &&
-            cardComb[i].getNumber() == cardComb[i+4].getNumber() - 4 && cardComb[i].getColor() == cardComb[i+4].getColor()
-        ) {
-            total++;
-        }
-    }        
-    return total > 0;
-}
-
-void StraightFlush::computeScore() {
-    int total;
-    if (isStraightFlush(this->Rules::getCards(), total)) {
-        if (total < 1) {
-            this->Rules::setScore(80);
+bool StraightFlush::isStraightFlush(std::vector<Card> cardComb, int& number, int& color) {
+    int size = this->Rules::getCombination();
+    int arr[size];
+    int arrColor[4];
+    for (int i = 0; i < size; i++)
+    {
+        arr[i]= this->Rules::getCard(i).getNumber();
+    }
+    for (int i = 0; i < 4; i++)
+    {
+        arrColor[i]= this->Rules::getCard(i).getColor();
+    }
+    int max = 13;
+    int maxColor = 4;
+    std::vector<int> result = countElements(arr, size, max);
+    std::vector<int> resultColor = countElements(arrColor, size, maxColor);
+    bool straightFlush = false;
+    number=0;
+    color=0;
+    for(int i = 0; i < maxColor; i++){ 
+        if(resultColor[i]==5){
+            color=i;
+            for(int j = 0; j < max; j++){ 
+                if(result[j]>0 && result[j+1]>0 && result[j+2]>0 && result[j+3]>0 && result[j+4]>0){
+                    straightFlush=true;
+                    number=j+4;
+                }
+            }
         }
     }
+    if (straightFlush) {
+        for (int i = 0; i < this->Rules::getCombination(); i++)
+        {
+            if (number < this->Rules::getCard(i).getNumber() && color < this->Rules::getCard(i).getColor()) {
+                number = this->Rules::getCard(i).getNumber();
+                color = this->Rules::getCard(i).getColor();
+            }
+            if (number == this->Rules::getCard(i).getNumber() && color < this->Rules::getCard(i).getColor()) {
+                color = this->Rules::getCard(i).getColor();
+            }
+            if (number < this->Rules::getCard(i).getNumber() && color == this->Rules::getCard(i).getColor()) {
+                number = this->Rules::getCard(i).getNumber();
+            }
+        }
+    }
+    return straightFlush;
 }
+void StraightFlush::computeScore() {
+    int number, color = 0;
+    float score = 0;
+    if (isStraightFlush(this->Rules::getCards(), number, color)) {
+        score = 80 + number * 0.1 + color * 0.3;
+    }
+    this->Rules::setScore(score > this->Rules::getScore() ? score : this->Rules::getScore());
+    
+}
+
+    
+
