fd813717efa66e30fa3d18a88e96daa652102cbd
afnanramadhan <13521011@std.stei.itb.ac.id>
2023-03-14 17:55:38 +0700

    edit ability

--- src/Class/Ability/Switch.cpp
+++ src/Class/Ability/Switch.cpp
@@ -8,7 +8,24 @@ Switch::Switch() : Ability::Ability(6){
 }
 
 
-void Switch::useAbility(int playerNumber, int enemyNumber){
+void Switch::useAbility(int playerNumber){
+    cout << ListPlayerVec::getPlayer(playerNumber).getNickname() << " melakukan Switch" << endl;
+    cout << "Kartumu sekarang adalah : "<<endl;
+    ListPlayerVec::getPlayer(playerNumber).print();
+    cout << "Silahkan pilih pemain yang kartunya ingin anda tukar:" << endl;
+    int count=1;
+    for(int i=0;i<ListPlayerVec::getPlayers().size();i++){
+        if(i != playerNumber){
+            cout<< count <<"." << ListPlayerVec::getPlayer(i).getNickname();
+            count++;
+        }
+    }
+    cout<<">";
+    int enemyNumber;
+    cin >> enemyNumber;
+    
+    cout<<"Kedua kartu " << ListPlayerVec::getPlayer(playerNumber).getNickname() <<" telah ditukar dengan "<< ListPlayerVec::getPlayer(enemyNumber).getNickname()<<"!";
+
     vector<Card> enemyCard,playerCard;
     enemyCard = this->ListPlayerVec::getPlayer(enemyNumber).getCards();
     playerCard = this->ListPlayerVec::getPlayer(playerNumber).getCards();
@@ -17,4 +34,7 @@ void Switch::useAbility(int playerNumber, int enemyNumber){
     this->ListPlayerVec::getPlayer(playerNumber).setCards(enemyCard);
 
     Ability::setUsingAbility(true);
+
+    cout << "Kartumu sekarang adalah : "<<endl;
+    ListPlayerVec::getPlayer(playerNumber).print();
 }
