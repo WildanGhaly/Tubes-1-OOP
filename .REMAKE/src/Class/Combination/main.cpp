ae8e2ef4c2bfa848cecac4f27d64f36545a1f97c
AlphaThrone <haikalardzi@gmail.com>
2023-03-14 09:35:36 +0700

    exception unfinished

--- .REMAKE/src/Class/Combination/main.cpp
+++ .REMAKE/src/Class/Combination/main.cpp
@@ -8,96 +8,108 @@
 #include "SubCombination/FullHouse/FullHouse.cpp"
 #include "SubCombination/FourOfKind/FourOfKind.cpp"
 #include "SubCombination/StraightFlush/StraightFlush.cpp"
+#include "../../Exception/Exception.h"
 
 int main(){
-    Combination *combination;
-    
-    Card c1(3, 1); // kiri warna, kanan angka
-    Card c2(0, 1); 
-    Card c3(0, 3);
-    Card c4(3, 3);
-    Card c5(0, 4);
-    Card c6(2, 4);
-    Card c7(0, 4);
-
+    int count=0;
     CardList<Card> cards = CardList<Card>();
-    cards << c1 << c2 << c3 << c4 << c5 << c6 << c7;
+    int color;
+    int number;
+    while(count<10){
+            count++;
+        try{
+            cout <<"Loop : " <<count << endl;
+            Combination *combination;
+            for(int i=0; i < 7; i++){
+                cout << "Color : ";
+                cin >> color;
+                cout << "Number: ";
+                cin >> number;
+                Card c(color, number);
+                cards << c;
+            }
 
-    cout << "=== Daftar Kartu ===" << endl;
-    cards.sortByNumberAndColor();
-    cards.print();
-    cout << "====================" << endl << endl;
 
-    HighCard highCard;
-    highCard.setCards(cards);
+            cout << "=== Daftar Kartu ===" << endl;
+            cards.sortByNumberAndColor();
+            cards.print();
+            cout << "====================" << endl << endl;
 
-    Pair pair;
-    pair.setCards(cards);
+            HighCard highCard;
+            highCard.setCards(cards);
 
-    TwoPair twoPair;
-    twoPair.setCards(cards);
+            Pair pair;
+            pair.setCards(cards);
 
-    ThreeOfKind threeOfKind;
-    threeOfKind.setCards(cards);
+            TwoPair twoPair;
+            twoPair.setCards(cards);
 
-    Straight straight;
-    straight.setCards(cards);
+            ThreeOfKind threeOfKind;
+            threeOfKind.setCards(cards);
 
-    Flush flush;
-    flush.setCards(cards);
+            Straight straight;
+            straight.setCards(cards);
 
-    FullHouse fullHouse;
-    fullHouse.setCards(cards);
+            Flush flush;
+            flush.setCards(cards);
 
-    FourOfKind fourOfKind;
-    fourOfKind.setCards(cards);
+            FullHouse fullHouse;
+            fullHouse.setCards(cards);
 
-    StraightFlush straightFlush;
-    straightFlush.setCards(cards);
+            FourOfKind fourOfKind;
+            fourOfKind.setCards(cards);
 
-    combination = &highCard;
-    combination->computeScore();
-    cout << "High Card      : " << combination->getPoint() << endl;
+            StraightFlush straightFlush;
+            straightFlush.setCards(cards);
 
-    combination = &pair;
-    combination->setPoint(highCard.getPoint());
-    combination->computeScore();
-    cout << "Pair           : " << combination->getPoint() << endl;
+            combination = &highCard;
+            combination->computeScore();
+            cout << "High Card      : " << combination->getPoint() << endl;
 
-    combination = &twoPair;
-    combination->setPoint(pair.getPoint());
-    combination->computeScore();
-    cout << "Two Pair       : " << combination->getPoint() << endl;
+            combination = &pair;
+            combination->setPoint(highCard.getPoint());
+            combination->computeScore();
+            cout << "Pair           : " << combination->getPoint() << endl;
 
-    combination = &threeOfKind;
-    combination->setPoint(twoPair.getPoint());
-    combination->computeScore();
-    cout << "Three of Kind  : " << combination->getPoint() << endl;
+            combination = &twoPair;
+            combination->setPoint(pair.getPoint());
+            combination->computeScore();
+            cout << "Two Pair       : " << combination->getPoint() << endl;
 
-    combination = &straight;
-    combination->setPoint(threeOfKind.getPoint());
-    combination->computeScore();
-    cout << "Straight       : " << combination->getPoint() << endl;
+            combination = &threeOfKind;
+            combination->setPoint(twoPair.getPoint());
+            combination->computeScore();
+            cout << "Three of Kind  : " << combination->getPoint() << endl;
 
-    combination = &flush;
-    combination->setPoint(straight.getPoint());
-    combination->computeScore();
-    cout << "Flush          : " << combination->getPoint() << endl;
+            combination = &straight;
+            combination->setPoint(threeOfKind.getPoint());
+            combination->computeScore();
+            cout << "Straight       : " << combination->getPoint() << endl;
 
-    combination = &fullHouse;
-    combination->setPoint(flush.getPoint());
-    combination->computeScore();
-    cout << "Full House     : " << combination->getPoint() << endl;
+            combination = &flush;
+            combination->setPoint(straight.getPoint());
+            combination->computeScore();
+            cout << "Flush          : " << combination->getPoint() << endl;
 
-    combination = &fourOfKind;
-    combination->setPoint(fullHouse.getPoint());
-    combination->computeScore();
-    cout << "Four of Kind   : " << combination->getPoint() << endl;
+            combination = &fullHouse;
+            combination->setPoint(flush.getPoint());
+            combination->computeScore();
+            cout << "Full House     : " << combination->getPoint() << endl;
 
-    combination = &straightFlush;
-    combination->setPoint(fourOfKind.getPoint());
-    combination->computeScore();
-    cout << "Straight Flush : " << combination->getPoint() << endl;
+            combination = &fourOfKind;
+            combination->setPoint(fullHouse.getPoint());
+            combination->computeScore();
+            cout << "Four of Kind   : " << combination->getPoint() << endl;
 
-    return 0;
+            combination = &straightFlush;
+            combination->setPoint(fourOfKind.getPoint());
+            combination->computeScore();
+            cout << "Straight Flush : " << combination->getPoint() << endl;
+        } catch (CardColorException e){
+            cout << e.what();
+        } catch (CardNumberException e){
+            cout << e.what();
+        }
+    }
+    
 }
