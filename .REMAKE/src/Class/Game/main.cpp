80d50f1ab06ae7dcca853221168e6757d65ec088
Hidayatullah Wildan Ghaly Buchary <13521015@std.stei.itb.ac.id>
2023-03-14 17:31:12 +0700

    feat: NEW main for Game

---
+++ .REMAKE/src/Class/Game/main.cpp
@@ -0,0 +1,20 @@
+// #include "../Card/CardList.cpp"
+#include "../Player/PlayerList.cpp"
+#include "../Table/Table.cpp"
+#include "Game.cpp"
+
+int main(){
+    Game<Card> game(7, "POKER");
+    cout << "MASUK\n";
+    game.start(2);
+
+    game.nextRound();
+    game.nextRound();
+    game.nextRound();
+    game.nextRound();
+    game.nextRound();
+
+    game.print();
+
+    return 0;
+}
