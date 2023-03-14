1f0c9dcc3c2fa4faebc586be4caec35cc86f5b00
AlphaThrone <haikalardzi@gmail.com>
2023-03-14 15:23:11 +0700

    exception (again)

--- .REMAKE/src/Exception/Exception.h
+++ .REMAKE/src/Exception/Exception.h
@@ -7,49 +7,50 @@ using namespace std;
 class CardNumberException : public exception  {
 	public:
     const char* what() const throw() {
-		return "Exception: Invalid number\n";
+		return "\033[1;31mException: Invalid number\033[0m\n";
 	}
 };
 
 class CardColorException : public exception  {
 	public:
     const char* what() const throw() {
-		return "Exception: Invalid color\n";
+		return "\033[1;31mException: Invalid Color\033[0m\n";
 	}
 };
 
 class InvalidInputException : public exception  {
 	public:
     const char* what() const throw() {
-		return "Exception: Invalid input, Please try again!\n";
+		return "\033[1;31mException: Invalid input, Please try again!\033[0m\n";
 	}
 };
 
 class NameCharOutOfBounds : public exception  {
 	public:
     const char* what() const throw() {
-		return "Exception: Length Out of Bounds\n";
+		return "\033[1;31mException: Length Out of Bounds\033[0m\n";
+        
 	}
 };
 
-class RuntimeError : public exception  {
+class RuntimeError : public runtime_error  {
 	public:
     const char* what() const throw() {
-		return "Runtime Error\n";
+		return "\033[1;31mRuntime Error\033[0m\n";
 	}
 };
 
 class IndexOutOfBoundException : public exception  {
 	public:
     const char* what() const throw() {
-		return "Exception: Index out of bounds\n";
+		return "\033[1;31mException: Index out of bounds\033[0m\n";
 	}
 };
 
 class FileNotExistException : public exception  {
 	public:
     const char* what() const throw() {
-		return "Exception: No such filename\n";
+		return "\033[1;31mException: No such filename\033[0m\n";
 	}
 };
 
