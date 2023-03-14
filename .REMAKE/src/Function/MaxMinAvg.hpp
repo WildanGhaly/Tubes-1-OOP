2e4e712f95f4c4d2519dd6c04f43e640fded0b45
Hidayatullah Wildan Ghaly Buchary <13521015@std.stei.itb.ac.id>
2023-03-14 21:53:48 +0700

    feat: generic function for max min and avg

---
+++ .REMAKE/src/Function/MaxMinAvg.hpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+template <typename T>
+T find_max(T a, T b) {
+    return a > b ? a : b;
+}
+
+template <typename T>
+T find_min(T a, T b) {
+    return a < b ? a : b;
+}
+
+template <typename T>
+T find_avg(T a, T b) {
+    return (a + b) / 2;
+}
+
+template<typename T>
+T find_max(T arg) {
+    return arg;
+}
+
+template<typename T, typename... Args>
+T find_max(T arg, Args... args) {
+    return max(arg, find_max(args...));
+}
+
+template<typename T>
+T find_min(T arg) {
+    return arg;
+}
+
+template<typename T, typename... Args>
+T find_min(T arg, Args... args) {
+    return min(arg, find_min(args...));
+}
+
+template<typename T>
+T find_avg(T arg) {
+    return arg;
+}
+
+template<typename T, typename... Args>
+T find_avg(T arg, Args... args) {
+    return (arg + find_avg(args...)) / 2;
+}
