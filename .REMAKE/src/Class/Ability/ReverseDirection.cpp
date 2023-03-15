9ee8325024bc2cee7e918d85fd1aa886268565b5
afnanramadhan <13521011@std.stei.itb.ac.id>
2023-03-16 02:32:27 +0700

    edit setvalue ability

--- .REMAKE/src/Class/Ability/ReverseDirection.cpp
+++ .REMAKE/src/Class/Ability/ReverseDirection.cpp
@@ -8,9 +8,9 @@ ReverseDirection::ReverseDirection() : Ability::Ability(4){
 bool ReverseDirection::useAbility(Game<Card>& game, int playerAbility, int players){
     Player player;
     vector<Player> playerList;
-    if (playerAbility == 4){
+    if (playerAbility == 4 || playerAbility == 0 || playerAbility == -1){
         cout << "masuk" << endl;
-        if (ReverseDirection::isUsingAbility() == false){
+        if (playerAbility == 4){
             player = game.getPlayer(players);
             player.setAbility(-1);
             playerList = game.getPlayers();
@@ -18,15 +18,17 @@ bool ReverseDirection::useAbility(Game<Card>& game, int playerAbility, int playe
             reverse(playerList.begin() + players + 1, playerList.end());
             cout << "masuk" << endl;
             game.setPlayers(playerList);
+            game.setPlayer(players, player);
             cout << "masuk" << endl;
             Ability::setUsingAbility(true);
             return true;
-        } else {
+        } else if(playerAbility ==-1){
             printPesan2("REVERSE DIRECTION");
-            return false;
+        }else if(playerAbility == 0){
+            printPesan3("REVERSE DIRECTION");
         }
     } else {
         printPesan("REVERSE DIRECTION");
-        return false;
     }
+    return false;
 }
