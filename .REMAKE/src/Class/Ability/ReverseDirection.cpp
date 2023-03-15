c0ce702a06f331fbee5ec4b728fd43e3d3e51fd0
Hidayatullah Wildan Ghaly Buchary <13521015@std.stei.itb.ac.id>
2023-03-16 05:50:12 +0700

    style: remove debugger output for Reverse

--- .REMAKE/src/Class/Ability/ReverseDirection.cpp
+++ .REMAKE/src/Class/Ability/ReverseDirection.cpp
@@ -9,17 +9,13 @@ bool ReverseDirection::useAbility(Game<Card>& game, int playerAbility, int playe
     Player player;
     vector<Player> playerList;
     if (playerAbility == 4 || playerAbility == 0 || playerAbility == -1){
-        cout << "masuk" << endl;
         if (playerAbility == 4){
             player = game.getPlayer(players);
             player.setAbility(-1);
             playerList = game.getPlayers();
-            cout << "masuk" << endl;
             reverse(playerList.begin() + players + 1, playerList.end());
-            cout << "masuk" << endl;
             game.setPlayers(playerList);
             game.setPlayer(players, player);
-            cout << "masuk" << endl;
             Ability::setUsingAbility(true);
             return true;
         } else if(playerAbility ==-1){
