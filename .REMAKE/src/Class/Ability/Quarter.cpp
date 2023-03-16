ef49041edabb24f845ba6aaf57451fdeee0e0b46
afnanramadhan <13521011@std.stei.itb.ac.id>
2023-03-16 08:34:38 +0700

    edit id yang udh kepake

--- .REMAKE/src/Class/Ability/Quarter.cpp
+++ .REMAKE/src/Class/Ability/Quarter.cpp
@@ -8,15 +8,15 @@ Quarter::Quarter() : Ability::Ability(3){
 
 bool Quarter::useAbility(Game<Card>& game, int playerAbility, int player){
     Player players;
-    if (playerAbility == 3 || playerAbility == 0 || playerAbility == -1){
+    if (playerAbility == 3 || playerAbility == 0 || playerAbility == -3){
         if (playerAbility == 3){
             game.setReward(game.getValue() / 4);
             players = game.getPlayer(player);
-            players.setAbility(-1);
+            players.setAbility(-3);
             game.setPlayer(player, players);
             Ability::setUsingAbility(true);
             return true;
-        } else if(playerAbility ==-1){
+        } else if(playerAbility ==-3){
             printPesan2("QUARTER");
         }else if(playerAbility ==0){
             printPesan3("QUARTER");
